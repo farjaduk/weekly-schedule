231b1619ff827113b1089632451eecc396c23724
Manning <farjad_uk@hotmail.com>
2021-11-22 11:13:51 -0500

    added variables

---
+++ Develop/script.js
@@ -0,0 +1,4 @@
+var todaysDate = ""; // string for holding date index to timeEntries
+var todaysDateString = ""; // string for holding today's date
+var currentHour = 9; // current hour for highlighting the correct row (default to first hour)
+var timeEntries = []; // starts list of log entries
